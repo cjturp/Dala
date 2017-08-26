351805e54f9acafe1cd2fa5835d1145cf77d47a9
Christopher Turpin <cjturpin1@gmail.com>
2017-08-26 13:55:16 -0400

    just made a shell for the page for users to choose their avatars

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
 
   get 'users/new'
 
-  get 'layouts/application'
-
+  get 'users/choose_avatar'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
