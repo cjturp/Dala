4abed0918c593998f1016be9276c0c6bca92d6c4
Your Name <email@example.com>
2017-08-31 15:54:38 -0400

    users can now be queried, see user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,39 @@ class User < ApplicationRecord
 
   has_many :user_answers
 
-  def
-    @user = User.where({
-      @
+  def exp_points
+    condition_hash = {
+      possible_answers: {
+        correct_ans: true
+      }
+    }
+    join_array = [:possible_answer]
+    self.user_answers.where(
+      condition_hash
+    ).includes(
+      join_array
+    ).sum("exp_points")
+  end
 
-
-      })
+  def current_level(game)
+    condition_hash = {
+      possible_answers: {
+        questions:{
+          game_levels: {
+            game: game
+          }
+        }
+      }
+    }
+    join_hash = {
+        possible_answer:{
+          question: :game_level
+        }
+    }
+    self.user_answers.where(
+      condition_hash
+    ).includes(
+      join_hash
+    ).first.possible_answer.question.game_level.level||1
   end
 end
--- app/models/user_answer.rb
+++ app/models/user_answer.rb
@@ -1,5 +1,4 @@
 class UserAnswer < ApplicationRecord
   belongs_to :user
-  has_one :possible_answer
-
+  has_one :possible_answer, foreign_key: :id
 end
